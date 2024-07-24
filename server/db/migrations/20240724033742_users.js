b430b7e447c7b3a74399dc2b2b36238ce07621c1
Sarah Situ <sarahsitu563@gmail.com>
2024-07-24 15:50:26 +1200

    Create migration file for users :

---
+++ server/db/migrations/20240724033742_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+    return knex.schema.createTable('users,', (table) => {
+        table.increments('id').primary()
+        table.string('auth0_id').unique().notNullable
+        table.string('email')
+        table.string('name')
+    })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+  return knex.schema.dropTable('users')
+};
