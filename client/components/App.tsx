0e1126dc55817ffb897ddecab7480d15bd0b6611
Sarah Situ <sarahsitu563@gmail.com>
2024-08-19 16:08:38 +1200

    Create ExpenseList component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,15 +1,17 @@
-import { useFruits } from '../hooks/useFruits.ts'
+import ExpenseList from "./ExpenseList"
 
 function App() {
-  const { data } = useFruits()
+  // const { data } = useFruits()
 
   return (
     <>
       <div className="app">
         <h1 className="text-3xl font-bold underline">
-          Fullstack Boilerplate - with Fruits!
+         Expenses
         </h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
+        <section>
+          <ExpenseList/>
+        </section>
       </div>
     </>
   )
