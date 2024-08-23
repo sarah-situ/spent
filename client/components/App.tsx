ef95742e03f8b057177a2b76b5a455c85454432e
Sarah Situ <sarahsitu563@gmail.com>
2024-08-23 14:05:01 +1200

    Remove add expense and expense list link in App.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -10,12 +10,6 @@ function App() {
       <div className="app">
      
         <section>
-        <nav>
-        <ul>
-          <li><Link to="/expense-form">Add Expense</Link></li>
-          <li><Link to="/expense-list">View Expenses</Link></li>
-        </ul>
-      </nav>
         <Outlet />
         </section>
       </div>
